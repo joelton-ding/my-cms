2af3154760f8eb30bec2a7646cfda11687858dbd
Joelton <joelton.ding@bwoil.com>
2019-06-27 13:54:27 +0800

    add email added store

--- src/components/AddEmail/FillEmail.js
+++ src/components/AddEmail/FillEmail.js
@@ -10,14 +10,40 @@ function handleChange(value) {
 }
 
 const FillEmail = () => {
-  let { state: { professionData }, actions: {}} = EmailListStore
+  let { state: { countryData, professionData, jobData, industryData }} = EmailListStore
   console.log('EmailListStore >>>>>', JSON.parse(JSON.stringify(EmailListStore)))
   console.log('professionData >>>>>', JSON.parse(JSON.stringify(professionData)))
-  let jsxOption = null
-  if(professionData && professionData.length>0){
-    jsxOption = professionData.map((item, index) => {
+
+  let countryOption = null
+  let professionOption = null
+  let jobOption = null
+  let industryOption = null
+  
+  if(countryData && countryData.length > 0){
+    countryOption = countryData.map((item, index) => {
+      return (
+        <Option key={index} value={item.label}>{item.label}</Option>
+      )
+    })
+  }
+  if(professionData && professionData.length > 0){
+    professionOption = professionData.map((item, index) => {
       return(
         <Option key={index}>{item.label}</Option>
+      )
+    })
+  }
+  if(jobData && jobData.length > 0){
+    jobOption = jobData.map((item, index) => {
+      return (
+        <Option key={index} value={item.label}>{item.label}</Option>
+      )
+    })
+  }
+  if(industryData && industryData.length > 0){
+    industryOption = industryData.map((item, index) => {
+      return(
+        <Option key={index} value={item.label}>{item.label}</Option>
       )
     })
   }
@@ -43,8 +69,7 @@ const FillEmail = () => {
                 onChange={handleChange}
                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
               >
-                <Option value="Singapore">Singapore</Option>
-                <Option value="Malaysia">Malaysia</Option>
+              { countryOption }
               </Select>  
             </td>
           </tr>
@@ -59,16 +84,7 @@ const FillEmail = () => {
                 onChange={handleChange}
                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
               >
-          
-              {/* {professionData.map((item, index) => {
-                return(
-                  <Option key={index}>{item.label}</Option>
-                )
-              })} */}
-
-              { jsxOption }
-            
-
+              { professionOption }
               </Select>  
             </td>
           </tr>
@@ -83,15 +99,7 @@ const FillEmail = () => {
                 onChange={handleChange}
                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
               >
-                <Option value="Executive">Executive</Option>
-                <Option value="Non-Executive">Non-Executive</Option>
-                <Option value="Entrepreneur">Entrepreneur</Option>
-                <Option value="Senior Management">Senior Management</Option>
-                <Option value="CEO/COO">CEO/COO</Option>
-                <Option value="CFO/CTO/CMO">CFO/CTO/CMO</Option>
-                <Option value="Director/VP">Director/VP</Option>
-                <Option value="Manager">Manager</Option>
-                <Option value="Others">Others</Option>
+                { jobOption }
               </Select> 
             </td>
           </tr>
@@ -106,35 +114,7 @@ const FillEmail = () => {
                 onChange={handleChange}
                 filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
               >
-                <Option value="Arts/Entertainment">Arts/Entertainment</Option>
-                <Option value="Airlines/Hospitality/Tourism/Travel">Airlines/Hospitality/Tourism/Travel</Option>				  
-                <Option value="Audit/Accountancy">Audit/Accountancy</Option>				  
-                <Option value="Banking/Finance">Banking/Finance</Option>				  
-                <Option value="Business Services/Consulting">Business Services/Consulting</Option>	
-                <Option value="Building/Construction">Building/Construction</Option>				  
-                <Option value="Day Trader">Day Trader</Option>				  
-                <Option value="Education/Training">Education/Training</Option>				  
-                <Option value="Engineering/Architecture">Engineering/Architecture</Option>				  
-                <Option value="Government/Uniform/Nonprofit">Government/Uniform/Nonprofit</Option>	
-                <Option value="Industrial/Manufacturing">Industrial/Manufacturing</Option>				  
-                <Option value="Information Technology">Information Technology</Option>				  
-                <Option value="Insurance">Insurance</Option>				  
-                <Option value="Internet/E-Commerce">Internet/E-Commerce</Option>				  
-                <Option value="Health Care">Health Care</Option>				  
-                <Option value="Legal">Legal</Option>				  
-                <Option value="Life Sciences/Medical">Life Sciences/Medical</Option>				 
-                <Option value="Marketing/Sales">Marketing/Sales</Option>				  
-                <Option value="Others">Others</Option>				  
-                <Option value="Real Estates">Real Estate</Option>				  
-                <Option value="Research/Development">Research/Development</Option>				  
-                <Option value="Retailer/Distributor/Wholesaler(computer related)">Retailer/Distributor/Wholesaler(computer related)</Option>				  
-                <Option value="Retailer/Distributor/Wholesaler">Retailer/Distributor/Wholesaler</Option>				  
-                <Option value="Retired">Retired</Option>				  
-                <Option value="Self Employed">Self Employed</Option>				  
-                <Option value="Student/NS">Student/NS</Option>				  
-                <Option value="Telecommunication">Telecommunication</Option>				  
-                <Option value="Transportation/Utilities">Transportation/Utilities</Option>	
-                <Option value="Unemployed">Unemployed</Option>
+              { industryOption } 
               </Select>  
             </td>
           </tr>
