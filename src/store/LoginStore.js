fdd31a0cba468970030ae614134d65dd8ff5e902
Joelton <joelton.ding@bwoil.com>
2019-07-08 14:27:45 +0800

    added .gitignore

--- src/store/LoginStore.js
+++ src/store/LoginStore.js
@@ -8,6 +8,53 @@ const state = observable({
   }
 })
 
+// //observable
+// const person = observable({
+//   firstName: 'Matt',
+//   lastName: 'Ruby',
+//   age: 37
+// })
+
+// console.log(person.firstName)
+
+// person.firstName = 'Mike'
+// console.log(person.firstName)
+
+// person.firstName = 'Jimmy'
+// console.log(person.firstName)
+
+// //Computed
+// const person1 = observable({
+//   firstName: 'Matt',
+//   lastName: 'Ruby',
+//   age: 0,
+//   fullName: function(){
+//     return this.firstName + ' ' + this.lastName;
+//   }
+// })
+// console.log(person1.fullName)
+
+// person1.firstName = 'Mike'
+// console.log(person1.fullName)
+
+// person1.firstName = 'Jimmy'
+// console.log(person1.fullName)
+
+// const person = observable({
+//   firstName: 'Matt',
+//   lastName: 'Ruby',
+//   age: 0
+// })
+
+// autorun(function(){
+//   console.log(person.firstName + ' ' + person.age)
+// })
+
+// const _Hello = '1'
+// _Hello.times(10, function(){
+//   person.age = _Hello.random(40)
+// })
+
 const actions = {}
 
 actions.showChecked = action((checked) => {
